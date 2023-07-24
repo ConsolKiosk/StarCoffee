d74648645fd52c81c64b2ef0738f890bceb66323
pmet1993 <temp19@naver.com>
2023-07-24 19:58:47 +0900

    test01

--- app/src/main/java/com/example/starcoffee/StarMain.kt
+++ app/src/main/java/com/example/starcoffee/StarMain.kt
@@ -1,5 +1,24 @@
 package com.example.starcoffee
 
 fun main() {
+    val shackBurger = ShackBurger("shackburger", 10.0)
+    shackBurger.displayInfo()
+    println(shackBurger.name)
+    println(shackBurger.price)
+
+    val calculate = Calculate()
+
+    calculate.productList.add(shackBurger.price)
+
+    println(calculate.currentMoney)
+    println(calculate.productList)
+    calculate.currentSum()
+    println(calculate.afterPayMoney)
+
+
+
+
+
 
 }
+
