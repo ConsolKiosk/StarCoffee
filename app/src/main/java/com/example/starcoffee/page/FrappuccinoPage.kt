6325cb1cdce5c84cca58779499f10a81dbc84741
pmet1993 <temp19@naver.com>
2023-07-26 01:21:55 +0900

    1.1 merge

--- app/src/main/java/com/example/starcoffee/page/FrappuccinoPage.kt
+++ app/src/main/java/com/example/starcoffee/page/FrappuccinoPage.kt
@@ -6,18 +6,18 @@ class FrappuccinoPage {
         val turnDrinkpage = DrinkPage()
 
         println("[ Frappuccino MENU ]")
-        for ((index, i) in noncoffeeList.withIndex()) {
+        for ((index, i) in frappuccinoList.withIndex()) {
             println("${index + 1}. ${i.name} ｜ W ${i.price} ｜ ")
         }
         println("0. 뒤로 가기 ｜ 뒤로 가기")
         println("메뉴를 골라 입력해주세요")
         val commandNumber = readLine()?.trim()?.toIntOrNull() ?: 0
         if (commandNumber == 0 ) {turnDrinkpage.drinkPage()}
-        else if (commandNumber > noncoffeeList.size) {turnDrinkpage.drinkPage()}
+        else if (commandNumber > frappuccinoList.size) {turnDrinkpage.drinkPage()}
         else {
-            productList.add(noncoffeeList[commandNumber - 1].name)
-            productPriceList.add(noncoffeeList[commandNumber - 1].price)
-            println("${noncoffeeList[commandNumber - 1].name}(을/를) W ${noncoffeeList[commandNumber - 1].price}에 구매하였습니다!!")
+            productList.add(frappuccinoList[commandNumber - 1].name)
+            productPriceList.add(frappuccinoList[commandNumber - 1].price)
+            println("${frappuccinoList[commandNumber - 1].name}(을/를) W ${frappuccinoList[commandNumber - 1].price}에 구매하였습니다!!")
             println("메인 페이지로 돌아갑니다.")
             println("현재 구매 내역 ${productList}")
             mainpage.mainPage()
