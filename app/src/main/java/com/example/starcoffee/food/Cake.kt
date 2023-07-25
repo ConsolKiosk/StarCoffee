4408936da8f3d9f85af5d9798e0b3ab2e22ee7f0
kyunghwa <snoopn303@gmail.com>
2023-07-25 19:44:16 +0900

    Menu/Food 클래스 업데이트

--- app/src/main/java/com/example/starcoffee/food/Cake.kt
+++ app/src/main/java/com/example/starcoffee/food/Cake.kt
@@ -6,5 +6,20 @@ open class  Cake : Food() {
 
 class CarrotCake : Cake() {
     override var name: String = "당근케이크"
-    override var price: Double = 5.0
+    override var price: Double = 6.5
+}
+
+class LemonCake : Cake() {
+    override var name: String = "레몬케이크"
+    override var price: Double = 6.8
+}
+
+class CheeseCake : Cake() {
+    override var name: String = "치즈케이크"
+    override var price: Double = 7.9
+}
+
+class ChocoCake : Cake() {
+    override var name: String = "초코케이크"
+    override var price: Double = 5.9
 }
