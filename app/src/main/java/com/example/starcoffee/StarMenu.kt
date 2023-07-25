6f715a0d05d76088e762cf933b92297f88759f74
rising111 <ejttotto1234@gmail.com>
2023-07-25 20:35:06 +0900

    IceOrHot기능 구현

--- app/src/main/java/com/example/starcoffee/StarMenu.kt
+++ app/src/main/java/com/example/starcoffee/StarMenu.kt
@@ -1,10 +1,13 @@
 package com.example.starcoffee
 
+import com.example.starcoffee.drink.Drink
 import com.example.starcoffee.food.CarrotCake
 
 fun main() {
     // var food = CarrotCake()
     // println( food.category)
+//    var drink = Drink()
+//    drink.IceOrHot() <--IceOrHot 구현
 }
 
 abstract class StarMenu {
