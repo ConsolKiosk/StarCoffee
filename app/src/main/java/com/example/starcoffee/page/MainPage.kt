16f486eb1977691a5db8c91ca1f049ac8a90fc35
sinw212 <sinw212@gmail.com>
2023-07-26 15:13:59 +0900

    Lv4 장바구니, 계산 로직 추가

--- app/src/main/java/com/example/starcoffee/page/MainPage.kt
+++ app/src/main/java/com/example/starcoffee/page/MainPage.kt
@@ -1,39 +1,34 @@
 package com.example.starcoffee.page
-
-import com.example.starcoffee.main
-
-
 class MainPage() {
     fun mainPage(){
-        var calculate = Calculate()
+//        var calculate = Calculate()
         val turnDrinkpage= DrinkPage()
         val turnFoodpage = FoodPage()
         val turnMdpage = MdPage()
+        val turnOrderpage = OrderPage()
 
-        println("[ STARCOFFEE MENU ]")
-        println("1. Drink            ｜")
-        println("2. Food             ｜")
-        println("3. MD               ｜")
-        println("0. 종료              ｜ 프로그램 종료")
-
-        calculate.currentSum()
-        println("메뉴를 골라 입력해주세요")
         while (true) {
+            println("★★★★★★★★★★★★★★★★★★★★")
+            println("STARCOFFEE 에 오신걸 환영합니다.")
+            println("★★★★★★★★★★★★★★★★★★★★")
+            println("아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.\n")
+            println("[ STARCOFFEE MENU ]")
+            println("1. Drink            ｜")
+            println("2. Food             ｜")
+            println("3. MD               ｜")
+            if(!orderLists.isEmpty()) { //장바구니에 물건이 있을때만 출력
+                println("0. Order            | 장바구니 확인 후 주문")
+            }
+//        calculate.currentSum()
+            println("메뉴를 골라 입력해주세요")
             val commandNumber = readLine()?.trim()?.toIntOrNull() ?: 999
-
             when (commandNumber) {
-                999 -> {
-                    println("다시 입력해주세요!")
-                    continue}
                 1 -> turnDrinkpage.drinkPage()
                 2 -> turnFoodpage.foodPage()
                 3 -> turnMdpage.mdPage()
-//                4 ->
-                0 -> {println("종료되었습니다!!")
-                    break}
+                0 -> turnOrderpage.orderPage()
+                else -> println("mainPage 다시 입력해주세요!")
             }
         }
-
-
     }
 }
--- app/src/main/java/com/example/starcoffee/page/MdPage.kt
+++ app/src/main/java/com/example/starcoffee/page/MdPage.kt
@@ -2,9 +2,6 @@ package com.example.starcoffee.page
 
 class MdPage {
     fun mdPage() {
-        val turnMainpage = MainPage()
-
-
         println("[ MD MENU ]")
         for ((index, i) in mdList.withIndex()) {
             println("${index + 1}. ${i.category}")
@@ -13,43 +10,52 @@ class MdPage {
         println("메뉴를 골라 입력해주세요")
         while (true) {
             val commandNumber = readLine()?.trim()?.toIntOrNull() ?: 999
-
             when (commandNumber) {
-                999 -> {
-                    println("다시 입력해주세요!")
-                    continue}
-                1 -> mdChoicePage(0)
-                2 -> mdChoicePage(1)
-                3 -> mdChoicePage(2)
-                4 -> mdChoicePage(3)
-                0 -> turnMainpage.mainPage()
-
+                in 1..4 -> {
+                    mdChoicePage(commandNumber-1)
+                    return
+                }
+                0 -> return
+                else -> println("다시 입력해주세요!")
             }
         }
-    } // fun drinkPage()
+    } // fun mdPage()
     fun mdChoicePage(k : Int) {
-        var choiceList = mutableListOf(accessoriesList, mugList, tumblerList, viaList)
+        var choiceMdList = mutableListOf(accessoriesList, mugList, tumblerList, viaList)
 
-        val mainpage = MainPage()
-        val turnMdpage = MdPage()
-
-        println("[ ${choiceList[k][0].category} MENU ]")
-        for ((index, i) in choiceList[k].withIndex()) {
+        println("[ ${choiceMdList[k][0].category} MENU ]")
+        for ((index, i) in choiceMdList[k].withIndex()) {
             println("${index + 1}. ${i.name} ｜ W ${i.price} ｜ ")
         }
         println("0. 뒤로 가기 ｜ 뒤로 가기")
         println("메뉴를 골라 입력해주세요")
-        val commandNumber = readLine()?.trim()?.toIntOrNull() ?: 0
-        if (commandNumber == 0 ) {turnMdpage.mdPage()}
-        else if (commandNumber > choiceList[k].size) {turnMdpage.mdPage()}
-        else {
-            productList.add(choiceList[k][commandNumber - 1].name)
-            productPriceList.add(choiceList[k][commandNumber - 1].price)
-            println("${choiceList[k][commandNumber - 1].name}(을/를) W ${choiceList[k][commandNumber - 1].price}에 구매하였습니다!!")
-            println("메인 페이지로 돌아갑니다.")
-            println("현재 구매 내역 ${productList}")
-            mainpage.mainPage()
-
+        while(true) {
+            val mdNum = readLine()?.trim()?.toIntOrNull() ?: 999
+            when(mdNum) {
+                in 1..4 -> {
+                    println("${choiceMdList[k][mdNum - 1].name} | W ${choiceMdList[k][mdNum - 1].price}")
+                    println("위 메뉴를 장바구니에 추가하시겠습니까?")
+                    while(true) {
+                        println("1. 확인, 2. 취소")
+                        var listAddNum = readLine()?.trim()?.toIntOrNull() ?: 999
+                        when(listAddNum) {
+                            1 -> {
+                                println("${choiceMdList[k][mdNum - 1].name} 가 장바구니에 추가되었습니다.")
+                                orderLists.put(choiceMdList[k][mdNum-1].name, choiceMdList[k][mdNum-1].price)
+                                println("메인 페이지로 돌아갑니다.")
+                                return
+                            }
+                            2 -> {
+                                println("취소하셨습니다. 메인 페이지로 돌아갑니다.")
+                                return
+                            }
+                            else -> println("다시 입력해주세요.")
+                        }
+                    }
+                }
+                0 -> return
+                else -> println("다시 입력해주세요.")
+            }
         }
     }
 }
